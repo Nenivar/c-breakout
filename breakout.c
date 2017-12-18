1151225778b29c518a1875caa18a30c8481d44d3
Nenivar <nenivar@gmail.com>
2017-12-18 10:08:02 +0000

    Grid

--- breakout.c
+++ breakout.c
@@ -2,14 +2,21 @@
 #include <stdio.h>
 
 #include "display.h"
+//#include "grid.h"
+#include "grid.c"
 
 void fail (char *message) {
     fprintf (stderr, "%s\n", message);
     exit (1);
 }
 
+void test () {
+    testGrid ();
+}
+
 int main (int n, char *args [n]) {
-    display ();
+    //display ();
+    test ();
 
     return 1;
 }
